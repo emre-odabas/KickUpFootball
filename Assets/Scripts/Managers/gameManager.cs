f40769faae5bb07ce985ccbd0908fa925eb1bce2
odabas32 <emreodabas_32@hotmail.com>
2020-01-11 20:43:18 +0300

    Skills system düzeltmeler yapıldı

--- Assets/Scripts/Managers/gameManager.cs
+++ Assets/Scripts/Managers/gameManager.cs
@@ -19,6 +19,9 @@ public class gameManager : MonoBehaviour {
 
     [Header ("REFERANSLAR")]
     public GameObject refSpawnParticle;
+    public GameObject refSkill;
+
+    public skill[] skills;
 
     //Özellikler
     public GameState gameState { get; set; } = GameState.Wait;
@@ -28,40 +31,39 @@ public class gameManager : MonoBehaviour {
 
     }
 
-    void Start () {
-
-    }
-
-    void Update () {
-
-    }
-
     void FixedUpdate () {
         _gameState = gameState;
     }
 
     public void gameEnd () {
         gameState = GameState.End;
+        StopCoroutine ("IESpawnSkillParticle");
     }
 
     public void spawnSkill () {
-        StartCoroutine ("IESpawnSkill");
+        StartCoroutine ("IESpawnSkillParticle");
     }
 
-     IEnumerator IESpawnSkill () {
-        Debug.Log ("spawnskill aktif");
-        //rndSkill
+    //skillden önce particle effect i verip ondan sonra skill i spawnlıyoruz
+    private IEnumerator IESpawnSkillParticle () {
+        float slillDuration = 5;
         Vector2 camPos = Camera.main.ScreenToWorldPoint (Vector2.zero);
-
         while (true) {
-            if (gameState == GameState.Start) {
-                yield return new WaitForSeconds (5);
-                float rndX = Random.Range (-camPos.x - 1, camPos.x + 1);
-                float rndY = Random.Range (-camPos.y - 5, camPos.y + 1);
-                GameObject particle = Instantiate (refSpawnParticle, new Vector2 (rndX, rndY), Quaternion.identity);
-                Debug.Log ("skill spawn oldu");
-            }
+            yield return new WaitForSeconds (slillDuration);
+            float rndX = Random.Range (-camPos.x - 1, camPos.x + 1);
+            float rndY = Random.Range (-camPos.y - 5, camPos.y + 1);
+            GameObject particle = Instantiate (refSpawnParticle, new Vector2 (rndX, rndY), Quaternion.identity);
+            slillDuration = Random.Range (5, 15);
+            Debug.Log ("Skill spawn oluyor. Bir sonraki skill " + slillDuration + " saniye sonra spawn olacak.");
+
+            StartCoroutine ("IESpawnSkill", new Vector2 (rndX, rndY));
         }
+    }
+
+    private IEnumerator IESpawnSkill (Vector2 skillPos) {
+        yield return new WaitForSeconds (3);
+        GameObject itemObject = Instantiate (refSkill, new Vector2 (skillPos.x, skillPos.y), Quaternion.identity);
+        itemObject.transform.GetComponent<SpriteRenderer> ().sprite = skills[0].sprite;
     }
 
 }
---
+++ Assets/Scripts/Skil/skill.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skill/Skill Item")]
+public class skill : ScriptableObject
+{  
+    public Sprite sprite; // resmi
+    public float duration; // kaç saniye ekranda kalacağı
+    public string propertyTag; // Özellik etiketine göre topun bu tag a sahip objeye değince olacak olan işlemler için
+}
